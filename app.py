bd4dc3b6908557efba232e94f129e4dc5ebf18fa
Vince <vincentm88@gmail.com>
2024-07-06 13:17:37 -0400

    added opengraph meta data call and preload into cards 50 at a time load more button at the bottom

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
-from scraper import load_config, fetch_article_html, parse_articles_from_html
+from flask import Flask, render_template, jsonify
+from scraper import load_config, fetch_article_html, parse_articles_from_html, fetch_open_graph_metadata
 
 app = Flask(__name__)
-
+articles = []
 
 @app.route('/')
 def index():
-    articles = []
     config = load_config('config.json')
 
     for site, details in config['sites'].items():
@@ -14,14 +13,23 @@ def index():
         print(f"Using selector: {details['link-selector']}")
         html = fetch_article_html(details['url'])
         if html:
-            article = parse_articles_from_html(details['url'], html, details['link-selector'], details['title-selector'])
+            articles_data = parse_articles_from_html(details['url'], html, details['link-selector'], details['title-selector'])
+            articles.extend(articles_data)
         else:
             print(f"Failed to retrieve content from {details['url']}")
-            
-        if article:
-            articles.extend(article)
-    print(f"Retrieved {len(articles)} articles")
+
     return render_template('index.html', articles=articles)
+
+@app.route('/metadata/<int:index>')
+def get_metadata(index):
+    try:
+        article = articles[index]
+        metadata = fetch_open_graph_metadata(article['link'])
+        if metadata:
+            article.update(metadata)
+        return jsonify(article)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
